ab5997b2c64270fc5f602ba981f4add65acfdbe6
Vinesh05 <59442706+Vinesh05@users.noreply.github.com>
2020-10-25 19:37:56 +0530

    Testing Connection

--- app/src/main/java/com/reaper/myapplication/MainActivity.kt
+++ app/src/main/java/com/reaper/myapplication/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.reaper.myapplication
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this,"Hello",Toast.LENGTH_SHORT).show()
 
     }
 
