77d04c970885df96a45bde306bc02b0655590c23
sohamparab <sohamparab1003@gmail.com>
2020-12-05 20:51:31 +0530

    Added app icon.

--- app/src/main/java/com/reaper/myapplication/activity/PlaylistSongs.kt
+++ app/src/main/java/com/reaper/myapplication/activity/PlaylistSongs.kt
@@ -2,11 +2,38 @@ package com.reaper.myapplication.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.reaper.myapplication.R
+import com.reaper.myapplication.adapter.PlaylistSongsAdapter
+import com.reaper.myapplication.databinding.ActivityMainBinding
+import com.reaper.myapplication.databinding.ActivityPlaylistSongsBinding
 
 class PlaylistSongs : AppCompatActivity() {
+    lateinit var playlistRecyclerView: RecyclerView
+    lateinit var binding: ActivityPlaylistSongsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playlist_songs)
+
+        binding = ActivityPlaylistSongsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        playlistRecyclerView = binding.playlistRecyclerView
+        playlistRecyclerView.layoutManager = LinearLayoutManager(this@PlaylistSongs)
+
+        val items = fetchData()
+        val adapter:PlaylistSongsAdapter = PlaylistSongsAdapter(items)
+        playlistRecyclerView.adapter=adapter
+
     }
+}
+
+private fun fetchData(): ArrayList<String> {
+    val list = ArrayList<String>()
+    for(i in 0 until 10){
+        list.add("Playlist $i")
+    }
+    return list
 }
---
+++ app/src/main/java/com/reaper/myapplication/adapter/PlaylistSongsAdapter.kt
@@ -0,0 +1,34 @@
+package com.reaper.myapplication.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.reaper.myapplication.R
+
+class PlaylistSongsAdapter(val items:ArrayList<String>): RecyclerView.Adapter<PlaylistSongsAdapter.PlaylistViewHolder>() {
+
+    class PlaylistViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        val cardView: CardView = itemView.findViewById(R.id.onlineCardView)
+        val txtPlaylistSongName: TextView = itemView.findViewById(R.id.txtSongName)
+        val txtPlaylistSongDuration: TextView = itemView.findViewById(R.id.txtDuration)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaylistViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.online_single_row,parent,false)
+        return PlaylistViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PlaylistViewHolder, position: Int) {
+        val currentItem = items[position]
+        holder.txtPlaylistSongName.text = currentItem
+        holder.txtPlaylistSongDuration.text = "0000"
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
