5cbc4403aaf631f8d66d55ecbca2a4cdfb87472d
sohamparab <sohamparab1003@gmail.com>
2021-03-25 18:24:15 +0530

    Added Music Control Features in Playlist Activity

--- app/src/main/java/com/reaper/myapplication/activity/PlaylistSongs.kt
+++ app/src/main/java/com/reaper/myapplication/activity/PlaylistSongs.kt
@@ -1,21 +1,30 @@
-package com.reaper.myapplication.activity
+ package com.reaper.myapplication.activity
 
 import android.content.Intent
+import android.media.Image
 import android.media.MediaMetadataRetriever
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.Gravity
+import android.view.View
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.transition.Slide
+import androidx.transition.Transition
+import androidx.transition.TransitionManager
 import com.reaper.myapplication.MusicApplication
 import com.reaper.myapplication.R
 import com.reaper.myapplication.adapter.MySongsAdapter
 import com.reaper.myapplication.database.PlayListById
 import com.reaper.myapplication.databinding.ActivityPlaylistSongsBinding
 import com.reaper.myapplication.utils.MySongInfo
+import kotlinx.android.synthetic.main.activity_playlist_songs.*
 import java.time.Duration
 
 class PlaylistSongs : AppCompatActivity() {
@@ -25,6 +34,14 @@ class PlaylistSongs : AppCompatActivity() {
     lateinit var totalDurationHours: TextView
     lateinit var totalDurationMinutes: TextView
     lateinit var totalDurationSeconds: TextView
+    lateinit var dragUpButtonPlaylist: ImageView
+    lateinit var dragDownbuttonPlaylist: ImageView
+    lateinit var ellipsePlaylist: ImageView
+    lateinit var playPlaylistBtn: ImageView
+    lateinit var pausePlaylistBtn: ImageView
+    lateinit var txtSongNamePlaylist: TextView
+    lateinit var txtArtistPlaylist: TextView
+    lateinit var relativeGroup:RelativeLayout
     lateinit var binding: ActivityPlaylistSongsBinding
     lateinit var applic: MusicApplication
     lateinit var act: MainActivity
@@ -47,6 +64,117 @@ class PlaylistSongs : AppCompatActivity() {
 
         playlistRecyclerView = binding.playlistRecyclerView
         playlistRecyclerView.layoutManager = LinearLayoutManager(this@PlaylistSongs)
+
+        dragUpButtonPlaylist = binding.dragUpButtonPlaylist
+        dragUpButtonPlaylist.visibility = View.INVISIBLE
+
+        dragDownbuttonPlaylist = binding.dragDownButtonPlaylist
+        dragDownbuttonPlaylist.visibility = View.INVISIBLE
+
+        ellipsePlaylist = binding.ellipsePlaylist
+        ellipsePlaylist.visibility = View.GONE
+
+        playPlaylistBtn = binding.playPlaylist
+        playPlaylistBtn.visibility = View.INVISIBLE
+
+        pausePlaylistBtn = binding.pausePlaylist
+        pausePlaylistBtn.visibility = View.INVISIBLE
+
+        txtSongNamePlaylist = binding.txtSongNamePlaylist
+        txtSongNamePlaylist.visibility = View.INVISIBLE
+
+        txtArtistPlaylist = binding.txtArtistPlaylist
+        txtArtistPlaylist.visibility = View.INVISIBLE
+
+        relativeGroup = binding.relativeGroup
+
+        when{
+            applic.currentMySongInfo!=null -> {
+                txtSongNamePlaylist.text = applic.currentMySongInfo?.name
+                txtSongNamePlaylist.isSelected = true
+                val artist = applic.currentMySongInfo?.artist
+                txtArtistPlaylist.text = artist
+                dragUpButtonPlaylist.visibility = View.VISIBLE
+            }
+            else -> {
+                dragUpButtonPlaylist.visibility= View.GONE
+                pausePlaylistBtn.visibility = View.GONE
+            }
+        }
+
+        dragUpButtonPlaylist.setOnClickListener {
+            dragUpButtonPlaylist.animate().apply {
+                duration = 300
+                rotationXBy(360f)
+            }.withEndAction {
+                val transition: Transition = Slide(Gravity.BOTTOM)
+                transition.duration = 300
+                transition.addTarget(R.id.onlineEllipse)
+                transition.addTarget(R.id.onlinePlay)
+                transition.addTarget(R.id.txtSongName)
+                transition.addTarget(R.id.txtDuration)
+                transition.addTarget(R.id.dragDownButton)
+                TransitionManager.beginDelayedTransition(relativeGroup, transition)
+                dragUpButtonPlaylist.visibility=View.GONE
+                txtSongNamePlaylist.visibility = View.VISIBLE
+                txtSongNamePlaylist.isSelected = true
+                txtArtistPlaylist.visibility = View.VISIBLE
+                ellipsePlaylist.visibility = View.VISIBLE
+                if(applic.musicIsPlaying){
+                    playPlaylistBtn.visibility=View.VISIBLE
+                    pausePlaylistBtn.visibility=View.GONE
+                }
+                else{
+                    playPlaylistBtn.visibility=View.GONE
+                    pausePlaylistBtn.visibility=View.VISIBLE
+                }
+                dragDownButtonPlaylist.visibility = View.VISIBLE
+            }
+        }
+
+        dragDownbuttonPlaylist.setOnClickListener {
+            dragDownbuttonPlaylist.animate().apply {
+                duration=300
+                rotationXBy(360f)
+            }.withEndAction {
+                val transition: Transition = Slide(Gravity.BOTTOM)
+                transition.duration = 300
+                transition.addTarget(R.id.onlineEllipse)
+                transition.addTarget(R.id.onlinePlay)
+                transition.addTarget(R.id.txtSongName)
+                transition.addTarget(R.id.txtDuration)
+                transition.addTarget(R.id.dragDownButton)
+                TransitionManager.beginDelayedTransition(relativeGroup, transition)
+                txtSongNamePlaylist.visibility=View.GONE
+                txtArtistPlaylist.visibility=View.GONE
+                dragUpButtonPlaylist.visibility=View.VISIBLE
+                ellipsePlaylist.visibility=View.GONE
+                playPlaylistBtn.visibility=View.GONE
+                pausePlaylistBtn.visibility= View.GONE
+                dragDownbuttonPlaylist.visibility=View.GONE
+            }
+        }
+
+        playPlaylistBtn.setOnClickListener {
+            playPlaylistBtn.visibility=View.GONE
+            pausePlaylistBtn.visibility=View.VISIBLE
+            applic.mediaPlayer.pause()
+            applic.musicIsPlaying = false
+        }
+
+        pausePlaylistBtn.setOnClickListener {
+            pausePlaylistBtn.visibility=View.GONE
+            playPlaylistBtn.visibility= View.VISIBLE
+            applic.mediaPlayer.start()
+            applic.musicIsPlaying = true
+        }
+
+        ellipsePlaylist.setOnClickListener {
+            dragDownbuttonPlaylist.callOnClick()
+            val intent = Intent(this@PlaylistSongs, SongActivity::class.java)
+            intent.putExtra("isLoaded",true)
+            startActivity(intent)
+        }
 
         val playlistInfo = PlayListById(this,playlistId).execute().get()
         val songUriList = playlistInfo.songList.split(",").map{it.trim()}
@@ -136,4 +264,9 @@ class PlaylistSongs : AppCompatActivity() {
         super.onResume()
     }
 
+    override fun onBackPressed() {
+        applic.mainActivity?.dragDownButton?.callOnClick()
+        applic.mainActivity?.dragUpButton?.callOnClick()
+        super.onBackPressed()
+    }
 }
